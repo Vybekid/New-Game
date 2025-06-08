8c0954dc4f1c79c0cef6bbbd90f6af4f07a4e1d7
Vybekid <felobitto@gmail.com>
2025-06-08 20:32:35 +0300

    lets goo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.transform import scale
 
 pygame.init()
 
@@ -9,12 +10,17 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Shooter')
 
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale, ):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/idle/O.png.jpg')
+        self.img = pygame.transform.scale(img, (float(img.get_width() * scale), float(img.get_height() * scale)))
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
 
-x = 200
-y = 200
-img = pygame.image.load('img/player/idle/O.png.jpg')
-rect = img.get_rect()
-rect.center = (x, y)
+
+player = Soldier(200,200,3)
+
 
 
 run = True
@@ -29,8 +35,6 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
-
-
-
+    pygame.display.update()
 
 pygame.quit()
